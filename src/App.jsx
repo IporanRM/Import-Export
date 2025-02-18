2fddfd1e149dc9ca1d4aace152778a830388a779
IporanRM <iporan122@gmail.com>
2025-02-17 22:25:08 -0300

    adicionando arquivo com varios componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+
 import Banner from "./components/Banner";
-import Header from "./components/Header";
+import Header from "./components/Header"; 
+import Component1, { Component2 } from "./components/VariosComponentes";
 
 const App = () => {
   return ( 
@@ -7,6 +9,8 @@ const App = () => {
     <>
     <Header />
     <Banner />
+    <Component1 />
+    <Component2 />
     </>
    );
 }
---
+++ src/components/VariosComponentes.jsx
@@ -0,0 +1,20 @@
+const Component1 = () => {
+    return ( 
+        <h1>Componente 1</h1>
+     );
+}
+ 
+export default Component1;
+
+export const Component2 = () => {
+    return ( 
+        <h1>Componente 2</h1>
+     );
+}
+
+export const Component3 = () => {
+    return ( 
+        <h1>Componente </h1>
+     );
+}
+ 
